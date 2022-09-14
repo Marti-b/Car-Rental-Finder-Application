13c08928f2b8e4f6ed2f9983cfd012730b973544
Balazs <martikanbalazs@gmail.com>
2022-09-14 23:43:31 +0200

    Add model, controller and install ef package

---
+++ Car Rental Finder/Car Rental Finder/Controllers/CarsController.cs
@@ -0,0 +1,21 @@
+﻿using Car_Rental_Finder.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Rental_Finder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private List<CarEntity> cars = new List<CarEntity>() {
+            new CarEntity() {Id= Guid.NewGuid(), Brand = "Honda", Color= "blue", HorsePower = 189, Price = 50000, YearOfProduction = 1999}
+        };
+
+        [HttpGet]
+        public IEnumerable<CarEntity> Get()
+        {
+            return cars;
+        }
+    }
+}
---
+++ Car Rental Finder/Car Rental Finder/Models/CarEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Car_Rental_Finder.Models
+{
+    public class CarEntity
+    {
+        [Key] public Guid Id { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+        public string Brand { get; set; }
+        public int HorsePower { get; set; }
+        public int YearOfProduction { get; set; }
+    }
+}
